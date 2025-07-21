cac3a2354a3af3d5fd41e1d42bd62d1d740dd2f7
LeshenShow <samuraiiron29@gmail.com>
2025-07-21 22:38:30 +0300

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Button variant="outline" color="blue">
         TEST
       </Button>
-      <div>Hello world . .. // cc/</div>
+      <div>Hello world . .. // sscc/</div>
     </div>
   )
 }
