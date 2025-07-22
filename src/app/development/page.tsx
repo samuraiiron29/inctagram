962e38ff353ac8724c84243d6981a7e99aeb39e2
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-07-23 00:17:52 +0300

    kan-30 chore: add RadioGroup to dev stand

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -4,8 +4,10 @@ import { useRouter } from 'next/navigation'
 export default function Page() {
   const router = useRouter()
   const redirectHandler = (kan: string) => router.push(`/development/${kan}`)
+
   return (
     <Flex gap={'2'} p={'5'}>
+      <Button children={'kan-10 radio-group'} onClick={() => redirectHandler('kan-10')} />
       <Button children={'kan-15 tabs'} onClick={() => redirectHandler('kan-15')} />
       <Button children={'kan-20 pagination'} onClick={() => redirectHandler('kan-20')} />
       <Button children={'kan-23 typography'} onClick={() => redirectHandler('kan-23')} />
