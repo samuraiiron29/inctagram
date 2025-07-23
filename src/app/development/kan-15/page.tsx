d21eaf8b47509f3aa8c9d0360f91d6feb259fe64
Ver-cel1995 <aleksei.tselikin@gmail.com>
2025-07-23 15:34:43 +0300

    feat: add checkbox

--- src/app/development/kan-15/page.tsx
+++ src/app/development/kan-15/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { TabsRadix } from '@/shared/ui/base/TabsRadix/TabsRadix'
 import TabsComponent, { Tab } from '@/shared/ui/base/Tabs/Tabs'
+import CustomCheckbox from '@/shared/ui/base/CheckBox/CheckBox'
+import { useState } from 'react'
 export default function Page() {
   const tab: any = [
     { id: '1', title: 'Home', content: 'Homefsdfsdafsadfasdfasdf', disabled: false },
     { id: '2', title: 'Dog', content: 'fdsf231231231', disabled: true },
     { id: '3', title: 'Cat', content: 'fsdaklk;l312318', disabled: false },
   ]
+
+  const [agree, setAgree] = useState(false);
   return (
     <div>
       <div style={{ marginTop: '40px' }}>
@@ -19,6 +23,10 @@ export default function Page() {
           <span>↓</span>
         </h1>
         <TabsRadix tabs={tab} width={'300px'} />
+      </div>
+
+      <div style={{ marginTop: '40px', paddingLeft: '20px' }}>
+        <CustomCheckbox onChange={setAgree} checked={agree} label={'dasdasd'}/>
       </div>
     </div>
   )
--- src/shared/ui/base/CheckBox/CheckBox.tsx
+++ src/shared/ui/base/CheckBox/CheckBox.tsx
@@ -1,4 +1,48 @@
-// type Props = {}
-export const CheckBox = () => {
-  return <div></div>
+import React from "react";
+import Image from 'next/image'
+
+type Props = {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  label?: string;
+  disabled?: boolean;
 }
+
+const CustomCheckbox = ({
+                                                         checked,
+                                                         onChange,
+                                                         label,
+                                                         disabled = false,
+                                                       }: Props) => {
+  return (
+    <label className="flex items-center cursor-pointer select-none gap-2">
+      <div
+        className={`
+          w-5 h-5 rounded border-2 flex items-center justify-center transition
+          ${checked ? "bg-amber-50 border-blue-600" : "border-gray-400"}
+          ${disabled ? "opacity-50 cursor-not-allowed" : ""}
+        `}
+        onClick={(e) => {
+          e.preventDefault();
+          if (!disabled) onChange(!checked);
+        }}
+        role="checkbox"
+        aria-checked={checked}
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (!disabled && (e.key === "Enter" || e.key === " ")) {
+            e.preventDefault();
+            onChange(!checked);
+          }
+        }}
+      >
+        {checked && (
+          <Image src={'/check.svg'} alt={'check'} width={100} height={100} />
+        )}
+      </div>
+      {label && <span className="text-sm">{label}</span>}
+    </label>
+  );
+};
+
+export default CustomCheckbox;
