88bffd574299aca7f3e972b8b4789f3317163b28
LeshenShow <leshenshow@gmail.com>
2025-07-23 09:34:21 +0300

    push kan23

--- src/app/development/kan-23/page.tsx
+++ src/app/development/kan-23/page.tsx
@@ -9,22 +9,26 @@ export default function Page() {
     <>
       <Button onClick={() => router.back()}>Назад</Button>
       <Flex gap={'3'} direction={'column'} m={'5'}>
-        <Button variant="classic">TEST</Button>
-        <Flex className=" " width={'150px'}>
-          123
-        </Flex>
         <p className="text-regular_link ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, porro perferendis doloremque, asperiores voluptates itaque alias
           sed excepturi quam ipsam suscipit, pariatur ad dicta placeat veniam? Officiis eaque animi quia.
         </p>
-        <p className="text-h1  ">Обычный текст</p>
-        <p className="text-bold_text14 ">Жирный текст</p>
-        <p className="text-linkSmall  ">Ссылка</p>
-        <p className="text-large ">Ссылка</p>
-        <div className="bg-accent-700 text-regular_text14 text-danger-300">test</div>
-        <div className="bg-accent-700 text-regular_text14  ">test</div>
-        <div className="bg-accent-700 text-regular_text14   ">test</div>
-        <div className="bg-accent-700 text-regular_text14   text-cyan-400 ">test</div>
+        <p className="text-h1  ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, porro perferendis doloremque, asperiores voluptates itaque alias
+          sed excepturi quam ipsam suscipit, pariatur ad dicta placeat veniam? Officiis eaque animi quia.
+        </p>
+        <p className="text-bold_text14 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, porro perferendis doloremque, asperiores voluptates itaque alias
+          sed excepturi quam ipsam suscipit, pariatur ad dicta placeat veniam? Officiis eaque animi quia.
+        </p>
+        <p className="text-linkSmall  ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, porro perferendis doloremque, asperiores voluptates itaque alias
+          sed excepturi quam ipsam suscipit, pariatur ad dicta placeat veniam? Officiis eaque animi quia.
+        </p>
+        <p className="text-large ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, porro perferendis doloremque, asperiores voluptates itaque alias
+          sed excepturi quam ipsam suscipit, pariatur ad dicta placeat veniam? Officiis eaque animi quia.
+        </p>
       </Flex>
     </>
   )
--- src/shared/config/theme/theme-provider.tsx
+++ src/shared/config/theme/theme-provider.tsx
@@ -5,9 +5,7 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   return (
     <Theme>
       {/* <ThemePanel /> */}
-      {/* <Flex height={'100vh'} className="bg-dark-700" style={{ backgroundColor: 'var(--dark-700)' }}> */}
       {children}
-      {/* </Flex> */}
     </Theme>
   )
 }
