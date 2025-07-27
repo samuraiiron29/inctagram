6b58f79f2a87f1c7c1cb8357f660a9c7170ece57
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-07-27 21:51:24 +0300

    chore: sidebar header position

--- src/shared/ui/base/Header/Header.tsx
+++ src/shared/ui/base/Header/Header.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Header = ({ isLoggedIn = false }: Props) => {
   return (
-    <header className={'relative z-100 border-b border-dark-300 box-border bg-dark-700'}>
+    <header className={'relative z-100 border-b border-dark-300 box-border'}>
       <div className={'mx-auto flex justify-between items-center max-w-[1280px] w-full h-[60px] px-[60px]'}>
         <Link href={'/'} className={'text-large accent-light-100'}>
           Inctagram
