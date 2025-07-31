ce9a8f11beb756b0851106041615cb62a349a2a7
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-07-31 23:54:17 +0300

    chore: sidebar, header, i18n

---
+++ src/shared/ui/base/LogOut/LogOut.tsx
@@ -0,0 +1,43 @@
+import { Button } from '@/shared/ui/base/Button/Button'
+import { Modal } from '@/shared/ui/Modal/Modal'
+import { useAppSelector } from '@/shared/lib/hooks/appHooks'
+import { selectAppEmail } from '@/store/slices/appSlice'
+import { PATH } from '@/shared/lib/path/path'
+import { useLogoutMutation } from '@/shared/api/authApi'
+
+type Props = {
+  showModal: boolean
+  setShowModal: (value: boolean) => void
+}
+
+export const LogOut = ({ showModal, setShowModal }: Props) => {
+  const email = useAppSelector(selectAppEmail)
+  const [logout] = useLogoutMutation()
+
+  const logoutHandler = async () => {
+    try {
+      await logout().unwrap()
+      window.location.replace(PATH.AUTH.LOGIN)
+    } catch (error) {
+      console.error('Logout failed:', error)
+    } finally {
+      setShowModal(false)
+    }
+  }
+
+  return (
+    <Modal open={showModal} onClose={() => setShowModal(false)} modalTitle={'Log Out'}>
+      <p className={'text-amber-50'}>Are you sure you want to log out {email}?</p>
+      <div className={''}>
+        <div className={'flex gap-[15px] mt-[18px]'}>
+          <Button variant={'outlined'} onClick={logoutHandler}>
+            Yes
+          </Button>
+          <Button variant={'primary'} onClick={() => setShowModal(false)}>
+            No
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
