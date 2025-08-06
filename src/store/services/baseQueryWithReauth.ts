2ec5494f50ee096cd5fe639505f8a8919d75c70f
Ver-cel1995 <aleksei.tselikin@gmail.com>
2025-08-06 09:14:54 +0300

    fix: fix-2

--- src/store/services/baseQueryWithReauth.ts
+++ src/store/services/baseQueryWithReauth.ts
@@ -1,49 +1,46 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { BaseQueryFn } from '@reduxjs/toolkit/query';
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
 import { deleteCookie, getCookie, setCookie } from '@/shared/lib/utils/cookieUtils'
 
 export const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
-
   const baseQuery = fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     credentials: 'include',
-    prepareHeaders: (headers) => {
+    prepareHeaders: headers => {
       const token = getCookie('accessToken')
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set('Authorization', `Bearer ${token}`)
       }
-      return headers;
+      return headers
     },
-  });
+  })
 
   // первый запрос
-  let result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions)
 
   // refresh
+  const nakedBase = fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    credentials: 'include',
+  })
+
   if (result.error && result.error.status === 401) {
-    const refreshResult = await baseQuery(
-      { url: 'auth/update-tokens', method: 'POST' },
-      api,
-      extraOptions
-    );
-
-
-
+    const refreshResult = await nakedBase({ url: 'auth/update-tokens', method: 'POST' }, api, extraOptions)
 
     if (refreshResult.data) {
-      const accessToken = (refreshResult.data as { accessToken: string })?.accessToken;
+      const accessToken = (refreshResult.data as { accessToken: string })?.accessToken
       if (accessToken) {
-        setCookie('accessToken', accessToken.trim(), 7);
+        setCookie('accessToken', accessToken.trim(), 7)
         // Повторяем запрос с новым токеном
-        result = await baseQuery(args, api, extraOptions);
+        result = await baseQuery(args, api, extraOptions)
         if (typeof window !== 'undefined') {
           window.location.reload()
         }
       }
     } else {
-      deleteCookie('accessToken');
+      deleteCookie('accessToken')
     }
   }
 
-  return result;
-};
+  return result
+}
