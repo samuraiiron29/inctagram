852159a33e6628a1b526220f6d2cd13dd5b99a5f
Ver-cel1995 <aleksei.tselikin@gmail.com>
2025-08-10 23:47:12 +0300

    fix: fix-3

--- src/app/users/profile/[userId]/page.tsx
+++ src/app/users/profile/[userId]/page.tsx
@@ -1,14 +1,27 @@
 import type { PublicProfile } from '@/shared/api'
 import UserProfile from '@/shared/ui/UserProfile/userProfile'
+import { notFound } from 'next/navigation'
 
 type Params = { userId: string }
 
 const UserPage = async ({ params }: { params: Promise<Params> }) => {
-  const { userId } = await params
-  const res = await fetch(`https://inctagram.work/api/v1/public-user/profile/${userId}`)
-  const profile: PublicProfile = await res.json()
+  try {
+    const { userId } = await params
+    const res = await fetch(`https://inctagram.work/api/v1/public-user/profile/${userId}`)
+    const profile: PublicProfile = await res.json()
 
-  return <UserProfile profile={profile} />
+    if (!res.ok) {
+      notFound()
+    }
+
+    if (!profile || !profile.userMetadata) {
+      notFound()
+    }
+
+    return <UserProfile profile={profile} />
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default UserPage
--- src/shared/ui/userProfile/userProfile.tsx
+++ src/shared/ui/userProfile/userProfile.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import { type PublicProfile } from '@/shared/api'
-import { Posts } from '../Posts/Posts'
 import ImageUploader from '@/shared/ui/Image/ImageUploader'
 import { usePathname, useRouter } from 'next/navigation'
 import { useState } from 'react'
+import Posts from '@/shared/ui/Posts/Posts'
 
 type Props = {
   profile: PublicProfile
 }
-export const UserProfile = (props: Props) => {
+const UserProfile = (props: Props) => {
   const pathname = usePathname()
   const router = useRouter()
 
@@ -29,3 +29,5 @@ export const UserProfile = (props: Props) => {
     </>
   )
 }
+
+export default UserProfile
