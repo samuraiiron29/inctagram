8386c05b6d7d70a03520e74692fb33e19d7da349
Алексей <aleksei.tselikin@gmail.com>
2025-09-23 12:23:23 +0300

    fix: fix 5

--- src/shared/config/i18n/LanguageProvider.tsx
+++ src/shared/config/i18n/LanguageProvider.tsx
@@ -4,11 +4,11 @@ import i18n from './i18n'
 
 export function LanguageProvider({ lng, children }: { lng: string; children: React.ReactNode }) {
   useEffect(() => {
-    // console.log('lng', lng)
-    // console.log('i18n.language', i18n.language)
-    // if (lng && i18n.language !== lng) {
-    //   i18n.changeLanguage(lng)
-    // }
+    console.log('lng', lng)
+    console.log('i18n.language', i18n.language)
+    if (lng && i18n.language !== lng) {
+      i18n.changeLanguage(lng)
+    }
   }, [lng])
   return <>{children}</>
 }
