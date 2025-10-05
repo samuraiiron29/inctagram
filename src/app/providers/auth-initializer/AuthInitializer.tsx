da6de77c6a5c87a2dbd21146d62cd2defda3f534
Ver-cel1995 <aleksei.tselikin@gmail.com>
2025-10-05 14:45:15 +0300

    fix: fix 8

--- src/app/providers/auth-initializer/AuthInitializer.tsx
+++ src/app/providers/auth-initializer/AuthInitializer.tsx
@@ -13,10 +13,27 @@ export function AuthInitializer({ children }: { children: React.ReactNode }) {
     const checkAuth = async () => {
       if (accessToken) {
         dispatch(setIsLoggedIn(true))
+
+        try {
+          const updateResponse = await fetch('https://inctagram.work/api/v1/auth/github/update-tokens', {
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+            credentials: 'include',
+          })
+
+          if (!updateResponse.ok) {
+            throw new Error('Ошибка обновления refreshToken')
+          }
+        } catch (error) {
+          console.error('Ошибка refreshToken', error)
+        }
       } else {
         try {
           const res = await fetch(window.location.href, { credentials: 'include' })
           const refreshTokenValid = res.headers.get('x-refresh-token-valid') === 'true'
+
           if (refreshTokenValid) {
             dispatch(setIsLoggedIn(true))
           } else {
